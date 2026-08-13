8ac0fb031825d963f739718a63c61766ac07a304
Kiwian <kiwiandoesthings@gmail.com>
2026-08-12 22:32:52 -0400

    get ao3 back up

--- Ao3Api.cs
+++ Ao3Api.cs
@@ -4,38 +4,43 @@ using System.Diagnostics;
 using System.Text;
 
 public class Ao3Api {
-	public void MapApiFunctions(WebApplication app) {
+	private Logger logger = new Logger("Ao3");
+
+    public void MapApiFunctions(WebApplication app) {
 		app.MapGet("/getao3storyid", async (HttpContext context, string storyTitle, int page) => {
-			Console.WriteLine("AO3 | Getting story ID from searching \"" + storyTitle + "\" on page " + page);
-			Console.WriteLine("AO3 | From " + context.Request.Headers["CF-Connecting-IP"].FirstOrDefault() + " ||| " + context.Request.Headers.UserAgent.ToString());
+			logger.INFO("Getting story ID from searching \"" + storyTitle + "\" on page " + page);
+
 			return GetAo3ApiResponse("search", [storyTitle, page.ToString()]);
 		});
 
 		app.MapGet("/getao3text", async (HttpContext context, int storyID, int page) => {
-			Console.WriteLine("AO3 | Getting text from storyID " + storyID + " on chapter " + page);
-			Console.WriteLine("AO3 | From " + context.Request.Headers["CF-Connecting-IP"].FirstOrDefault() + " ||| " + context.Request.Headers.UserAgent.ToString());
+			logger.INFO("Getting text from storyID " + storyID + " on chapter " + page);
+
 			return GetAo3ApiResponse("text", [storyID.ToString(), page.ToString()]);
 		});
 	}
 
-	public static IResult GetAo3ApiResponse(string file, string[] args) {
+	public IResult GetAo3ApiResponse(string file, string[] arguments) {
 		ProcessStartInfo start = new ProcessStartInfo();
 		start.FileName = "python";
-		string arguments = "\"C:\\Users\\Kiwian\\Downloads\\Github Repos\\kiwiapi\\" + file + ".py\" ";
-		foreach (string argument in args) {
-			arguments += "\"" + argument + "\" ";
+		string pythonFile = Path.Combine(Environment.CurrentDirectory, file + ".py");
+		if (!File.Exists(pythonFile)) {
+			logger.ERR("Could not find python file at " + pythonFile);
 		}
-		start.Arguments = arguments;
+		start.ArgumentList.Add(pythonFile);
+		foreach (string argument in arguments) {
+            start.ArgumentList.Add(argument);
+        }
 		start.UseShellExecute = false;
 		start.RedirectStandardOutput = true;
-		start.CreateNoWindow = true;
+        start.RedirectStandardError = true;
+        start.CreateNoWindow = true;
 		start.StandardOutputEncoding = Encoding.UTF8;
 
-		using (Process? process = Process.Start(start)) {
-			using (StreamReader reader = process!.StandardOutput) {
-				string result = reader.ReadToEnd();
-				return Results.Content(result, "application/json");
-			}
-		}
+		using Process? process = Process.Start(start);
+		using StreamReader reader = process!.StandardOutput;
+		string result = reader.ReadToEnd();
+
+		return Results.Content(result, "application/json");
 	}
 }
